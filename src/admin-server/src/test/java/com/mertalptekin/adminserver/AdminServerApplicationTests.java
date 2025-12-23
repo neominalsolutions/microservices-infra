c714dfcdd3310bd10060cc27f91b14b3e8f50ea1
Administrator <neominal.solutions@gmail.com>
2025-12-23 13:50:40 +0300

    Initial commit

---
+++ src/admin-server/src/test/java/com/mertalptekin/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mertalptekin.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
