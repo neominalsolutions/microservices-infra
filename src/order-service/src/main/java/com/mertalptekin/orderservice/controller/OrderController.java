d8e845c6435c07e9953e68f44771dc71b78526f6
Administrator <neominal.solutions@gmail.com>
2025-12-24 16:06:32 +0300

    Spring Cloud Microservices Collection

--- src/order-service/src/main/java/com/mertalptekin/orderservice/controller/OrderController.java
+++ src/order-service/src/main/java/com/mertalptekin/orderservice/controller/OrderController.java
@@ -4,8 +4,12 @@ import com.mertalptekin.orderservice.client.ProductClient;
 import com.mertalptekin.orderservice.request.OrderedProductDetailRequest;
 import com.mertalptekin.orderservice.response.OrderedProductDetailResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +21,11 @@ import java.util.UUID;
 @Slf4j
 public class OrderController {
     private final ProductClient productClient;
-    public OrderController(ProductClient productClient){
+    private final StreamBridge streamBridge;
+
+    public OrderController(ProductClient productClient, StreamBridge streamBridge){
         this.productClient = productClient;
+        this.streamBridge = streamBridge;
     }
     // api/v1/orders/123/orderDetails
     @GetMapping("{orderCode}/orderDetails")
@@ -28,6 +35,16 @@ public class OrderController {
         productIds[1] = UUID.randomUUID().toString(); // veri tabanında çekilmiş gibi simüle ettik
         log.info("order-service-request");
         return productClient.getOrderedProducts(new OrderedProductDetailRequest(productIds));
+    }
+
+
+    @PostMapping("sendKafka")
+    public ResponseEntity<String> sendMessageToKafka(){
+
+        Message<String> message = MessageBuilder.withPayload("Mesaj Gönderildi").build();
+        boolean isSend = streamBridge.send("submitOrder-out-0",message);
+
+        return ResponseEntity.ok(isSend ? "Mesaj teslim edildi":"Mesaj iletilemedi");
     }
 
     // find Ordered Product In DB
