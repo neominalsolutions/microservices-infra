21621deb606ccb78a536c1510bf16e5b61d0c51f
Administrator <neominal.solutions@gmail.com>
2025-12-25 00:23:09 +0300

    Spring Cloud Gateway Reactive Redis RateLimitter entegrasyonu

--- src/gateway/src/main/java/com/mertalptekin/gateway/GatewayApplication.java
+++ src/gateway/src/main/java/com/mertalptekin/gateway/GatewayApplication.java
@@ -2,12 +2,25 @@ package com.mertalptekin.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@EnableCaching // Spring Boot Cache yapısı aktif et
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange ->
+                Mono.just(exchange.getRequest()
+                        .getRemoteAddress()
+                        .getAddress()
+                        .getHostAddress());
+    }
 }
