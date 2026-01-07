7657ab221456cabc16b076bcd70e0af13221901d
neominal.solutions <neominal.solutions@gmail.com>
2026-01-07 14:44:32 +0300

    SAGA Service eklendi: Saga orchestration için servis, event, repository ve konfigürasyon dosyaları eklendi, OrderController güncellendi, ortam ve docker-compose ayarları eklendi

--- src/order-service/src/main/java/com/mertalptekin/orderservice/event/SubmitOrderEvent.java
+++ src/order-service/src/main/java/com/mertalptekin/orderservice/event/SubmitOrderEvent.java
@@ -5,5 +5,7 @@ import com.mertalptekin.orderservice.dto.OrderedProduct;
 import java.util.List;
 
 // orderCode -> üzerinden itemları sipariş ettik.
+// orderCode -> burada operasyonun takibini sağlamak için kullanılanan unique operasyon idsi
+// bunun yerine correlationId de kullanılabilir.
 public record SubmitOrderEvent(String orderCode, List<OrderedProduct> items) {
 }
